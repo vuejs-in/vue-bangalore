2fa9bcdab194e0cc89012e26501b494a2b96a03f
Nimit Bhargava <nimitbhargava@users.noreply.github.com>
2019-09-14 01:01:04 +0530

    Enhancement slides (#31)
    * fix - event show youtube embed height was ver small, made it similar to event talk
    
    * enhancement - adding deck in event show
    
    * update event - adding deck and recording links

--- .vuepress/components/EventShow.vue
+++ .vuepress/components/EventShow.vue
@@ -6,6 +6,10 @@
     </div>
     <div v-if="description" v-html="description" class="description"></div>
     <EventSpeaker :speaker="speaker" :bio="bio" />
+    <ItemRow v-if="deck">
+      <Icon name="deck" slot="icon" />
+      <a :href="deck" target="_blank">Speaker Deck</a>
+    </ItemRow>
     <ItemRow v-if="website">
       <Icon name="website" slot="icon" />
       <a :href="website" target="_blank">{{ website }}</a>
@@ -15,7 +19,7 @@
 
 <script>
 export default {
-  props: ['title', 'speaker', 'website', 'bio', 'description', 'recording'],
+  props: ['title', 'speaker', 'website', 'bio', 'description', 'recording', 'deck'],
   filters: {
     filename (value) {
       return value.split('/').pop()
@@ -31,5 +35,19 @@ export default {
 
 .description {
   margin-bottom: 1rem;
+}
+
+.recording {
+  width: 100%;
+  padding-top: 56.29%;
+  position: relative;
+}
+
+iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
 }
 </style>
