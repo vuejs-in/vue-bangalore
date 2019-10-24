c9469d45f53f93187e63b38c7a4a92f3b8e22f45
Nimit Bhargava <nimitbhargava@gmail.com>
2019-10-24 17:18:51 +0530

    enhancement - when no twitter handle provided, then don't show img for twitter

--- .vuepress/components/EventSpeaker.vue
+++ .vuepress/components/EventSpeaker.vue
@@ -1,6 +1,6 @@
 <template>
   <ItemRow>
-    <img slot="icon" :src="'//avatars.io/twitter/' + twitter" class="avatar">
+    <img v-if="twitter" slot="icon" :src="'//avatars.io/twitter/' + twitter" class="avatar">
     <a :href="'https://twitter.com/' + twitter" target="_blank">
       {{ name }}
     </a>
